bb08102ff0c6945eaaaa96bdf9c20ff80166d6eb
Max Mynter <max.mynter@mistral.ai>
2025-11-28 18:17:48 +0100

    I want to enter in insert mode

--- init.lua
+++ init.lua
@@ -882,8 +882,7 @@ require('lazy').setup({
         ['<C-p>'] = { 'select_prev' },
         ['<Up>'] = { 'select_prev', 'fallback' },
         ['<Down>'] = { 'select_next', 'fallback' },
-        ['<CR>'] = { 'accept' },
-        ['<C-y>'] = { 'accept', 'fallback' },
+        ['<C-y>'] = { 'accept' },
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
